208372197359b3f2577783f24af8db54b235df4a
Nick Endle <nick.endle@gmail.com>
2019-09-12 14:06:32 -0500

    Update JS
    Move vendor files to /vendor
    Bring in POGs and associated functions

--- source/theme.js
+++ source/theme.js
@@ -1,2 +1,4 @@
-//= require javascripts/jquery.waypoints.min.js
-//= require javascripts/sticky.min.js
+//= require javascripts/vendor/jquery.waypoints.min.js
+//= require javascripts/vendor/sticky.min.js
+//= require javascripts/store
+//= require javascripts/product-option-groups
