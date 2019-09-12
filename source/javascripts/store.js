355e53c52588f510f256bb8f62f843187767ada9
Nick Endle <nick.endle@gmail.com>
2019-09-12 14:26:45 -0500

    Add hex to rgb function for gradients

--- source/javascripts/store.js
+++ source/javascripts/store.js
@@ -1,5 +1,24 @@
 var inPreview = (/\/admin\/design/.test(top.location.pathname));
 
+function hexToRGB(h) {
+  let r = 0, g = 0, b = 0;
+
+  // 3 digits
+  if (h.length == 4) {
+    r = "0x" + h[1] + h[1];
+    g = "0x" + h[2] + h[2];
+    b = "0x" + h[3] + h[3];
+
+  // 6 digits
+  } else if (h.length == 7) {
+    r = "0x" + h[1] + h[2];
+    g = "0x" + h[3] + h[4];
+    b = "0x" + h[5] + h[6];
+  }
+
+  return "rgb("+ +r + "," + +g + "," + +b + ")";
+}
+
 var isGreaterThanZero = function(currentValue) {
   return currentValue > 0;
 }
